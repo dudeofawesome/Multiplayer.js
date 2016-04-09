f2d81595429a8d450d4d51033a881d372f92a282
Louis Orleans <louis@orleans.io>
2016-04-08 18:51:19 -0700

    More work on whiteboard demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,23 @@
-function hashCode (str) { // java String#hashCode
+var ctx;
+var pen = new Pen();
+var pens = [];
+var drawings = [];
+
+function Pen (id) {
+    this.id = id;
+    this.x = 0;
+    this.y = 0;
+    this.width = 20;
+    this.height = 20;
+    this.down = false;
+    this.color = 'red';
+    this.img = new Image();
+    this.img.src = '/images/pencil-white.png';
+    this.img.width = this.width;
+    this.img.height = this.height;
+}
+
+function hashCode (str) {
     var hash = 0;
     for (var i = 0; i < str.length; i++) {
         hash = str.charCodeAt(i) + ((hash << 5) - hash);
@@ -14,29 +33,65 @@ function intToRGB (i) {
     return '00000'.substring(0, 6 - c.length) + c;
 }
 
+function loop () {
+    requestAnimationFrame(loop);
+
+    ctx.clearRect(0, 0, ctx.width, ctx.height);
+
+    for (var i in drawings) {
+        ctx.strokeStyle = drawings[i].color;
+        ctx.beginPath();
+        if (drawings[i].path[0]) {
+            ctx.moveTo(drawings[i].path[0].x, drawings[i].path[0].y);
+        }
+        for (var j in drawings[i].path) {
+            ctx.lineTo(drawings[i].path[j].x, drawings[i].path[j].y);
+        }
+        ctx.stroke();
+    }
+
+    ctx.drawImage(pen.img, pen.x, pen.y - pen.height, pen.width, pen.height);
+
+    for (var i in pens) {
+        if (pens[i] && i !== pen.id) {
+            ctx.drawImage(pens[i].img, pens[i].x, pens[i].y - pens[i].height, pens[i].width, pens[i].height);
+        }
+    }
+}
+
 window.onload = function () {
     // Sets up and connects to the multiplayer server
-    var me = document.getElementById('me');
-    var mp = new Multiplayer(me.style, ['left', 'top']);
+    var mp = new Multiplayer(pen, ['x', 'y', 'color', 'down']);
     mp.addCallback('connect', (id) => {
-        me.style.backgroundColor = '#' + intToRGB(hashCode(id));
+        pen.color = '#' + intToRGB(hashCode(id));
+        pen.id = id;
     });
     mp.addCallback('newPlayer', (player) => {
-        let sprite = document.createElement('div');
-        sprite.id = player.id;
-        sprite.style.backgroundColor = '#' + intToRGB(hashCode(player.id));
-        document.body.appendChild(sprite);
-        mp.linkPlayerID(player.id, sprite.style);
+        var newPen = new Pen(player.id);
+        pens[player.id] = newPen;
+        mp.linkPlayerID(player.id, newPen);
     });
     mp.addCallback('deadPlayer', (id) => {
-        document.getElementsByTagName('body')[0].removeChild(document.getElementById(id));
+        pens[id] = undefined;
         mp.unlinkPlayerID(id);
     });
 
-    // Updates mul.me and square position on the screen
-    window.addEventListener('mousemove', function (event) {
-        // mul.update({ x:event.pageX, y:event.pageY })
-        me.style.left = event.pageX;
-        me.style.top = event.pageY;
+    ctx = new Canvas({color: 'rgb(45,45,45)', dblclick_fullscreen:true});
+
+    Input.bind('onmousedown', function () {
+        pen.down = true;
+        drawings.push({color: pen.color, path: []});
     });
+    Input.bind('onmouseup', function () {
+        pen.down = false;
+    });
+    Input.bind('onmousemove', function (x, y) {
+        pen.x = x;
+        pen.y = y;
+        if (pen.down) {
+            drawings[drawings.length - 1].path.push({x: x, y: y});
+        }
+    });
+
+    loop();
 };
